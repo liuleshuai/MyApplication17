0d11d2955f9355e8bf1b94c79dac0ece2aa7410f
liukuo <lk54321@yeah.net>
2018-02-06 17:57:02 +0800

    。。。

--- app/src/main/java/com/example/a67017/myapplication/activity/Main11Activity.java
+++ app/src/main/java/com/example/a67017/myapplication/activity/Main11Activity.java
@@ -1,9 +1,14 @@
 package com.example.a67017.myapplication.activity;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.example.a67017.myapplication.R;
@@ -11,7 +16,14 @@ import com.example.a67017.myapplication.customeview.CameraSurfaceView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import permissions.dispatcher.NeedsPermission;
+import permissions.dispatcher.OnNeverAskAgain;
+import permissions.dispatcher.OnPermissionDenied;
+import permissions.dispatcher.OnShowRationale;
+import permissions.dispatcher.PermissionRequest;
+import permissions.dispatcher.RuntimePermissions;
 
+@RuntimePermissions
 @Route(path = "/activity/11")
 public class Main11Activity extends AppCompatActivity {
 
@@ -53,7 +65,7 @@ public class Main11Activity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        mSurfaceView.start();
+        Main11ActivityPermissionsDispatcher.successPermissionWithPermissionCheck(this);
     }
 
     @Override
@@ -68,5 +80,43 @@ public class Main11Activity extends AppCompatActivity {
         if (mSurfaceView != null) {
             mSurfaceView.releaseCamera();
         }
+    }
+
+    @NeedsPermission({Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE})
+    void successPermission() {
+        mSurfaceView.start();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        Main11ActivityPermissionsDispatcher.onRequestPermissionsResult(this, requestCode, grantResults);
+    }
+
+    @OnShowRationale({Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE})
+    void onshowRationale(final PermissionRequest request) {
+        new AlertDialog.Builder(this)
+                .setMessage("申请权限")
+                .setPositiveButton("好的", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        request.proceed();
+                    }
+                }).setNegativeButton("拒绝", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                request.cancel();
+            }
+        }).show();
+    }
+
+    @OnPermissionDenied({Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE})
+    void deniedPermission() {
+        Toast.makeText(this, "已拒绝权限申请！", Toast.LENGTH_SHORT).show();
+    }
+
+    @OnNeverAskAgain({Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE})
+    void neverAskAgain() {
+        Toast.makeText(this, "不在询问权限申请！", Toast.LENGTH_SHORT).show();
     }
 }
