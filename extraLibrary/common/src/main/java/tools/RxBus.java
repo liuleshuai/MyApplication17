1e05b10967b5fd29495b07a10152ea0e63bafdb1
liukuo <lk54321@yeah.net>
2018-03-21 14:30:59 +0800

    Rxbus

---
+++ extraLibrary/common/src/main/java/tools/RxBus.java
@@ -0,0 +1,54 @@
+package tools;
+
+import io.reactivex.Flowable;
+import io.reactivex.processors.FlowableProcessor;
+import io.reactivex.processors.PublishProcessor;
+
+/**
+ * RxJava2 设置监听器，监听event
+ * <p>
+ * Created by LiuKuo at 2018/3/21
+ */
+
+public class RxBus {
+
+    /**
+     * 相当于一个事件队列，存储的是Flowable事件
+     */
+    private FlowableProcessor<Object> bus;
+
+    /**
+     * PublishSubject只会把在订阅发生的时间点之后来自原始Flowable的数据发射给观察者
+     */
+    public RxBus() {
+        bus = PublishProcessor.create().toSerialized();
+    }
+
+    public static RxBus getDefault() {
+        return RxBusHolder.Instance;
+    }
+
+    private static class RxBusHolder {
+        private static RxBus Instance = new RxBus();
+    }
+
+    /**
+     * 添加一个新的事件
+     *
+     * @param o Object
+     */
+    public void post(Object o) {
+        bus.onNext(o);
+    }
+
+    /**
+     * 过滤事件
+     * 过滤操作符 ofType
+     *
+     * @param eventType 类型
+     * @return Flowable<T>
+     */
+    public <T> Flowable<T> toFlowable(Class<T> eventType) {
+        return bus.ofType(eventType);
+    }
+}
