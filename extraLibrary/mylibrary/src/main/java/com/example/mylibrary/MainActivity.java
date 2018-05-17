b17c42f22048c0f227cf80cd246e6ec3e38de554
liuleshuai <670176775@qq.com>
2018-05-17 23:53:05 +0800

    eventbus

--- extraLibrary/mylibrary/src/main/java/com/example/mylibrary/MainActivity.java
+++ extraLibrary/mylibrary/src/main/java/com/example/mylibrary/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.mylibrary;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
+
+import org.greenrobot.eventbus.EventBus;
 
 @Route(path = "/libraryActivity/main")
 public class MainActivity extends AppCompatActivity {
@@ -12,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        EventBus.getDefault().register(this);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        EventBus.getDefault().unregister(this);
     }
 }
